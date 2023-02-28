b840d72a43be540bf6878dcd6e3be93b0d5888af
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-01 03:41:44 +0600

    add

---
+++ callback.js
@@ -0,0 +1,12 @@
+function greeting(greetingHandler) {
+  console.log(greetingHandler);
+}
+// const name = "Halim mama";
+// const number = [45, 54, 78];
+// const laptop = { price: 450000, brand: "lenovo", memory: "8gb" };
+
+function greetingHandler() {
+  console.log("Good morning");
+}
+
+greeting(laptop);
